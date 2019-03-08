8c31fd003c015050f0014eec770a51339a228aa9
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-08 11:30:08 -0800

    Add unsplash config api

---
+++ app/config/unsplash.js
@@ -0,0 +1,6 @@
+module.exports = {
+	accessKey: '5ba8ead3e49cf7972c4a7576326d29f2d272c32b07e04e476fedef0500067ac3',
+	secretKey: 'f42045073b6c2547d95f3ce890752763e01246f553b77e004c3ae437b817495b',
+	redirectUri: 'urn:ietf:wg:oauth:2.0:oob',
+	endpoint: 'https://api.unsplash.com/photos'
+}
