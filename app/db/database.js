6c49ac0e9d1a13ad3c8168f99efc9b9dca6326e0
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-08 11:31:14 -0800

    Add database config file

---
+++ app/db/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+	database: 'coffee_menu',
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'Qarrusel1!'
+}
---
+++ app/db/db-connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql')
+const dbOptions = require('./database')
+
+function databaseConnection () {
+	const db = mysql.createConnection(dbOptions)
+
+	db.connect(err => {
+		if (err) return console.error('There was an error connecting to the database')
+		else {console.log('Database successfully connected')}
+	})
+	return db
+}
+
+module.exports = databaseConnection()
