bbcd2a9d4ed2c1707c93f8e0e8a0d6f99ae9c4ef
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-09 07:46:41 -0800

    stuff

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -2,50 +2,61 @@ const db = require('../db/db-connection')
 const query = require('../queries/queries')
 
 function getProducts(req, res) {
-	db.query(query.getProducts, (err, data) => {
-		if (err) return res.status(500).send({message: `Error getting the products: ${err}`})
-		console.log(res.status)
 
-		return res.status(200).json({products: data[0]})
+	let promise = new Promise((resolve, reject) => {
+		db.query(query.getProducts, (err, data) => {
+			if (err) reject(err)
+
+			resolve(data[0])
+		})
 	})
+
+	promise
+		.then(data => { return res.status(200).json({ products: data }) })
+		.catch(err => { return res.status(500).send({ message: `Error getting the products: ${err}` }) })
 }
 
 function getProduct(req, res) {
-	const { productId } = req.params.product_id
-	console.log(productId)
+	const productId = req.params.product_id
 
 	db.query(query.getProduct, productId, (err, data) => {
+
 		if (err) return res.status(500).json({message: `Error getting the product: ${err}`})
+		else if (!data[0]) return res.status(404).json({message: 'Product not found!'})
+
 		return res.status(200).json({product: data[0]})
 	})
 }
 
 function getProductByName(req, res) {
-	const { productName } = req.body
+
+	const productName = req.body.product_name
 	console.log(productName)
 
 	db.query(query.getProductByName, productName, (err, data) => {
+
 		if (err) return res.status(500).json({message: `Error getting the product: ${err}`})
-		console.log(data[0])
+		else if (!data[0]) return res.status(404).json({message: 'Product not found!'})
 
 		return res.status(200).json({product: data[0]})
 	})
 }
 
 function getProductByCost(req, res) {
-	const { productCost } = req.body
+	const productCost = req.body.cost
 	console.log(productCost)
 
 	db.query(query.getProductByCost, productCost, (err, data) => {
+
 		if (err) return res.status(500).json({message: `Error getting the product: ${err}`})
-		console.log(data[0])
+		else if (!data[0]) return res.status(404).json({message: 'Product not found!'})
 
 		return res.status(200).json({product: data[0]})
 	})
 }
 
 function newProduct(req, res) {
-	const { product } = req.body
+	const product = req.body
 	console.log(product)
 
 	db.query(query.newProduct, product, (err, data) => {
@@ -61,12 +72,10 @@ function newProduct(req, res) {
 
 function updateProduct(req, res) {
 	const productId = req.params.product_id
-	const { product } = req.body
+	const product = req.body
 	console.log(productId)
 
-	console.log(product)
-
-	db.query(query.updateProduct, [productId, product], (err, data) => {
+	db.query(query.updateProduct, [product, productId], (err, data) => {
 		if (err) return res.status(500).json({message: `Error updating the product: ${err}`})
 		console.log(data[0])
 
