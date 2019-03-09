4cb7eb388d288d956347dd4862adea1d17548cf0
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-09 07:46:32 -0800

    Change the id params

--- app/routes/product.route.js
+++ app/routes/product.route.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const product = require('../controllers/product.controller')
 
 // Endpoints
-router.get('/products', product.getProducts)
-router.get('/products/:id', product.getProduct)
-router.get('/products/:name', product.getProductByName)
-router.get('/products/:cost', product.getProductByCost)
-router.post('/products', product.newProduct)
-router.put('/products/:id', product.updateProduct)
-router.delete('/products/:id', product.removeProduct)
+router.get('/products/name', product.getProductByName) // Get product by name
+router.get('/products/cost', product.getProductByCost) // Get product by cost
+router.get('/products', product.getProducts) // Get all products
+router.get('/products/:product_id', product.getProduct) // Get just one product
+router.post('/products', product.newProduct) // Create new product
+router.put('/products/:product_id', product.updateProduct) // Update an existing product
+router.delete('/products/:product_id', product.removeProduct) // Remove one product
 
 module.exports = router
