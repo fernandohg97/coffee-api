a083cfb21ce05c2e48f7cd4732e75559708a5a15
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-04-08 20:48:21 -0700

    Move unsplash logic from route to a controller

---
+++ app/controllers/unsplash.controller.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch')
+const checkStatus = require('../middlewares/checkStatus')
+
+function getPhotos(req, res) {
+
+	fetch(`${res.locals.unsplashUrl}&per_page=15&page=2`)
+		.then(checkStatus)
+		.then(body => {
+			res.send(body[0])
+		})
+		.catch(err => console.error(err))
+}
+
+function getPhotoByName(req, res) {
+
+	// Get the user search terms from the body
+	let userSearch = req.body
+	console.log(0 == false)
+
+	// Make the call to Unsplash API URL
+	if (Object.getOwnPropertyNames(userSearch).length != 0) { // In case the search form was filled by the user
+
+		fetch(`${res.locals.unsplashUrl}&query=${userSearch.keyword}`)
+			.then(response => response.json())
+			.then(body => res.send(body))
+			.catch(err => console.error(err))
+
+	} else { // In case the user doesn't type any value
+		res.status(400).json({message: 'You must enter a search value'})
+	}
+}
+
+module.exports = {
+	getPhotos,
+	getPhotoByName
+}
--- app/routes/unsplash.route.js
+++ app/routes/unsplash.route.js
@@ -1,39 +1,13 @@
 const express = require('express')
 const unsplashRouter = express.Router()
-const fetch = require('node-fetch')
 const setReqPath = require('../middlewares/auth').setReqPath
-const checkStatus = require('../middlewares/checkStatus')
+const unsplash = require('../controllers/unsplash.controller')
 
 // Endpoints
 // Get random photos
-unsplashRouter.get('/photos', setReqPath, (req, res) => {
-
-	fetch(`${res.locals.unsplashUrl}&per_page=15&page=2`)
-		.then(checkStatus)
-		.then(body => {
-			res.send(body[0])
-		})
-		.catch(err => console.error(err))
-})
+unsplashRouter.get('/photos', setReqPath, unsplash.getPhotos)
 
 // Search photos by user input
-unsplashRouter.get('/search/photos', setReqPath, (req, res) => {
-
-	// Get the user search terms from the body
-	let userSearch = req.body
-	console.log(0 == false)
-
-	// Make the call to Unsplash API URL
-	if (Object.getOwnPropertyNames(userSearch).length != 0) { // In case the search form was filled by the user
-
-		fetch(`${res.locals.unsplashUrl}&query=${userSearch.keyword}`)
-			.then(response => response.json())
-			.then(body => res.send(body))
-			.catch(err => console.error(err))
-
-	} else { // In case the user doesn't type any value
-		res.status(400).json({message: 'You must enter a search value'})
-	}
-})
+unsplashRouter.get('/search/photos', setReqPath, unsplash.getPhotoByName)
 
 module.exports = unsplashRouter
