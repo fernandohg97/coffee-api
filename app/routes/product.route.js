9737a4bfbacedccf37fc33f2fa2a360dc14f3f77
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-04-14 22:20:41 -0700

    Add endpoint to remove all products

--- app/routes/product.route.js
+++ app/routes/product.route.js
@@ -4,11 +4,11 @@ const product = require('../controllers/product.controller')
 
 // Endpoints
 productRouter.get('/products/name', product.getProductByName) // Get product by name
-// productRouter.get('/products/cost', product.getProductByCost) // Get product by cost
 productRouter.get('/products', product.getProducts) // Get all products
 productRouter.get('/products/:product_id', product.getProduct) // Get just one product
 productRouter.post('/products', product.newProduct) // Create new product
 productRouter.put('/products/:product_id', product.updateProduct) // Update an existing product
 productRouter.delete('/products/:product_id', product.removeProduct) // Remove one product
+productRouter.delete('/products', product.removeProducts) // Remove all products
 
 module.exports = productRouter
