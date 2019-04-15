106a6183ec28f2ace8cc65111cb1e675285d1ad1
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-04-14 22:19:39 -0700

    Put together in a nested json object

--- app/queries/queries.js
+++ app/queries/queries.js
@@ -1,28 +1,25 @@
-const product = {
-	getProducts: 'call getAllProducts();',
-	getProduct: 'call getOneProduct(?);',
-	getProductByName: 'call getProductByName(?);',
-	newProduct: 'call newProduct(?, ?, ?);',
-	updateProduct: 'call updateProduct(?, ?, ?, ?);',
-	removeProduct: 'call removeProduct(?);'
+const dbQueries = {
+	product: {
+		getProducts: 'call getAllProducts();',
+		getProduct: 'call getOneProduct(?);',
+		getProductByName: 'call getProductByName(?);',
+		newProduct: 'call newProduct(?, ?, ?);',
+		updateProduct: 'call updateProduct(?, ?, ?, ?);',
+		removeProduct: 'call removeProduct(?);',
+		removeProducts: 'call removeAllProducts();'
+	},
+	variant: {
+		newVariant: 'call newVariant(?);',
+	},
+	variant_values: {
+		newVariantValues: 'call newVariantValues(?, ?, ?);',
+		updateVariantValues: 'call updateVariantValues(?, ?, ?);'
+	},
+	sku: {
+		newSku: 'call newSku(?, ?);',
+		updateSku: 'call updateSku(?, ?);'
+	}
 }
 
-const variant = {
-	newVariant: 'call newVariant(?);'
-}
 
-const sku = {
-	newSku: 'call newSku(?, ?);'
-}
-
-const variantValues = {
-	newVariantValues: 'call newVariantValues(?, ?, ?);'
-}
-
-
-module.exports = {
-	product,
-	variant,
-	sku,
-	variantValues
-}
+module.exports = dbQueries
