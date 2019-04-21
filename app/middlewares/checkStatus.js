2f46ecb932d75deb05ceb0f64ef7c0fd621a5279
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-04-21 13:18:47 -0700

    Add condition in case the url includes users, handling the response for an error

--- app/middlewares/checkStatus.js
+++ app/middlewares/checkStatus.js
@@ -1,7 +1,10 @@
 module.exports = function checkStatus(unsplashRes) {
 	if (unsplashRes.ok) {
 		return unsplashRes.json()
-	} else {
+	} else if (unsplashRes.url.includes('users')) {
+		return new Error('User not found!')
+	}
+	else {
 		throw new Error(`There was an error getting the photos: ${unsplashRes.statusText}`)
 	}
 }
