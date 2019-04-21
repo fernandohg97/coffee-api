9d5786072eeba0a58cad2e22ba5465e2a1d5d828
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-04-21 13:19:32 -0700

    Add two endpoints, one to validate if username is filled and assign to request params and other for getting the photos by username

--- app/routes/unsplash.route.js
+++ app/routes/unsplash.route.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const unsplashRouter = express.Router()
 const setReqPath = require('../middlewares/auth').setReqPath
+const checkUsername = require('../middlewares/checkUsername')
 const unsplash = require('../controllers/unsplash.controller')
 
 // Endpoints
 // Get random photos
 unsplashRouter.get('/photos', setReqPath, unsplash.getPhotos)
 
-// Search photos by user input
+// Get photos by keyword
 unsplashRouter.get('/search/photos', setReqPath, unsplash.getPhotoByName)
+
+// Search username and redirect to below endpoint
+unsplashRouter.get('/users', checkUsername)
+
+// Get photos by user
+unsplashRouter.get('/users/:username/photos', setReqPath, unsplash.getUserPhotos)
 
 module.exports = unsplashRouter
