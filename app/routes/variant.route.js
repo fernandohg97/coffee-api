d18d2f9ea740a390296a878dd2115b5631133bd7
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-05-30 16:11:29 -0700

    Fix some routes and add middleware to create a new sku route

--- app/routes/variant.route.js
+++ app/routes/variant.route.js
@@ -3,9 +3,12 @@ const variantRouter = express.Router()
 const variantCtrl = require('../controllers/variant.controller')
 const { check } = require('express-validator/check')
 const errorHandler = require('../middlewares/errors/error.validation').handleErrorValidation
+const getValueIds = require('../middlewares/getValueNames')
 
 // ENPOINTS
 variantRouter.get('/variants', variantCtrl.getVariants) // Get all variants name
+
+variantRouter.get('/variants/values', variantCtrl.getVariantValues) // Get all variant values
 
 variantRouter.get('/variants/:variant_id', variantCtrl.getVariant) // Get variant name by variant id
 
@@ -18,21 +21,25 @@ variantRouter.post('/variants', [
 variantRouter.post('/variants/product/:product_id', [
 	check('value_name').isAlphanumeric(),
 	check('variant_id').isInt(),
-], errorHandler, variantCtrl.addVariant) // Add new product variant value
+], errorHandler, variantCtrl.newVariantValue) // Add new product variant value
 
-variantRouter.post('/variants/price/:product_id', [
+variantRouter.post('/variants/price/:product_id', getValueIds, [
+	check('sku').not().isEmpty(),
 	check('price').isFloat()
-], errorHandler, variantCtrl.addPrice) // Set price to product
+], errorHandler, variantCtrl.newSku) // Set price to product
 
 variantRouter.put('/variants/:variant_id', [
 	check('variant_name').not().isEmpty()
 ], errorHandler, variantCtrl.updateVariant) // Update a variant name
 
-variantRouter.put('/variants/product/:product_id', [
+variantRouter.put('/variants/product/:value_id', [
 	check('value_name').isAlphanumeric(),
 	check('variant_id').isInt(),
+], errorHandler, variantCtrl.updateVariantValue) // Update an exisitng variant value from a product
+
+variantRouter.put('/variants/price/:product_id/sku/:sku_id', [
 	check('price').isFloat()
-], errorHandler, variantCtrl.updateVariantValue) // Update an exisitng variant from a product
+], errorHandler, variantCtrl.updateSku) // Update price or sku to an existing variant value from a product
 
 variantRouter.delete('/variants/value/:value_id', variantCtrl.removeVariantValue) // Remove existing variant from a product
 
