8fee222f81e5a3658df4c781d83f587314cb4d81
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-05-30 16:09:57 -0700

    Fix newSku method, newVariantValue method, getVariants by product method. Also add method to get all variant values

--- app/controllers/variant.controller.js
+++ app/controllers/variant.controller.js
@@ -31,31 +31,58 @@ function getVariant(req, res) {
 	})
 }
 
+// Method to get all variant values
+function getVariantValues(req, res) {
+
+	let { getVariantValues } = query.variant_values
+
+	db.query(getVariantValues, (err, data) => {
+
+		if (err) return res.status(500).send({ message: `Error getting variant values: ${err}` })
+		else if (!data[0].length) return res.status(404).send({ message: 'Variants not found!' })
+
+		return res.status(200).send({ variants_values: data[0] })
+	})
+}
+
 // Method to get all variants in group by product_id
 function getVariantsByProduct(req, res) {
 
-	let { getVariantValues } = query.variant_values
+	let { getVariantValuesByProduct } = query.variant_values
 	let { product_id } = req.params
 
-	db.query(getVariantValues, product_id, (err, data) => {
+	db.query(getVariantValuesByProduct, product_id, (err, data) => {
 
 		if (err) return res.status(500).send({ message: `Error from server: ${err}` })
-		else if (!data[0].length) return res.status(404).send({ message: `Variant values not found for product_id: ${product_id}` })
+		else if (!data) return res.status(404).send({ message: `Variant values not found for product_id: ${product_id}` })
 
-		let variantValues = data[0] // Assign retrieved data from database
+		console.log(data)
+
+		let variantValues = data // Assign retrieved data from database
 		let groupVariants = [] // Array to group variants of the same product
 		let isEqual = true // Determine if enters the condition of differente variant_id
 
-		for (let i = 0; i < variantValues.length; i++) {
-			for (let j = 1; j < variantValues.length - 1; j++) {
+		// Manipulate database result set
+		// Join variant values according to certain conditions
+		// Validate product_id and variant_id to set variant values as one group.
+		// Final result: [{'12oz', 'vainilla'}, {'12oz', 'normal'}]
+		for (let i = 0; i < variantValues.length; i++) { // Loop result set starting from position 0
+			for (let j = 1; j <= variantValues.length - 1; j++) { // Loop result set starting from position 1
+
+				// In case product_id from first loop is equal to product_id from second loop and variant_id from first loop is different to variant_id from second loop
 				if (variantValues[i].product_id == variantValues[j].product_id && variantValues[i].variant_id != variantValues[j].variant_id) {
-					groupVariants.push(new Object(variantValues[i].value_name, variantValues[j].value_name))
+					// Multiple variable declaration
+					let variantId1 = variantValues[i].variant_id, valueName1 = variantValues[i].value_name, variantId2 = variantValues[j].variant_id, valueName2 = variantValues[j].value_name
+					let manyVariants = { [variantId1]: valueName1, [variantId2]: valueName2 } // Create an object with the variant_id as property name and value name as property value
+					groupVariants.push(manyVariants)
 					isEqual = false
 				}
 			}
-			if (isEqual) groupVariants.push(new Object(variantValues[i].value_name))
+			if (isEqual) {
+				let oneVariant = { [variantValues[i].variant_id]: variantValues[i].value_name }
+				groupVariants.push(oneVariant) // Add oneVariant object
+			}
 		}
-
 		return res.status(200).send({ variants: groupVariants })
 	})
 }
@@ -74,21 +101,14 @@ function newVariant(req, res) {
 	})
 }
 
-
-
-// Method to add variant to a product
-function addVariantValue(req, res) {
+// Method to create new variant value to an existing product
+function newVariantValue(req, res) {
 
 	let { newVariantValues } = query.variant_values
-	// let { newSku } = query.sku
 	let { value_name, variant_id } = req.body
 	let { product_id } = req.params
 
-	// let sql = mysql.format(`${newVariantValues} ${newSku}`, [value_name, product_id, variant_id, price, product_id])
-	let sql = mysql.format(`${newVariantValues}`, [value_name, product_id, variant_id])
-
-
-	db.query(sql, (err, data) => {
+	db.query(newVariantValues, [value_name, product_id, variant_id], (err, data) => {
 
 		if (err) return res.status(500).send({ message: `There was an error creating the product variant: ${err}` })
 
@@ -96,17 +116,52 @@ function addVariantValue(req, res) {
 	})
 }
 
+// Method to create a sku and price for an existing product
 function newSku(req, res) {
 
-	let { newSku } = query.sku
+	let { newSku, newSkuValues } = query.sku
 	let { sku, price } = req.body
 	let { product_id } = req.params
 
-	db.query(newSku, [sku, price, product_id], (err, data) => {
+	// Transaction
+	db.beginTransaction(err => {
+		if (err) return res.status(500).send({ message: `Hubo un error al iniciar la transaccion: ${err}` })
 
-		if (err) return res.status(500).send({ message: `Error setting the price to product: ${err}` })
+		db.query(newSku, [sku, price, product_id], (err, result) => {
 
-		return res.status(200).send({ message: 'Sku & price successfully created '})
+			if (err) return db.rollback(err) // Error creating the sku and price
+
+			let insertSkuId = result[1][0]['@insertId'] // Last inserted sku id
+			let concatSkuIds = req.valuesId.map(e => e.toString().split()) // Each element parse to String and each element split it into an array
+			concatSkuIds.map(e => e.splice(0,0, insertSkuId)) // Concatenate the last inserted sku id at the first position in each array element
+			let skuIdsValuesIds = concatSkuIds.map(e => e.map(Number)) // Each element of the arrays parse it to Number
+
+			db.query(newSkuValues, [skuIdsValuesIds], (error, data) => {
+
+				if (error) return res.status(500).send({ message: `Hubo un error al crear sku_values: ${error}` })
+
+				db.commit(function(err) {
+					if (err) db.rollback(err)
+					res.status(200).send({ message: 'Transaction complete!' })
+					db.end()
+				})
+			})
+		})
+	})
+}
+
+// Method to update sku and price from a product
+function updateSku(req, res) {
+
+	let { updateSku } = query.sku
+	let { sku, price } = req.body
+	let { product_id, sku_id } = req.params
+
+	db.query(updateSku, [sku_id, sku, price, product_id], (err, data) => {
+
+		if (err) return res.status(500).send({ message: `Error updating the price to product: ${err}` })
+
+		return res.status(200).send({ message: 'Sku & price successfully updated' })
 	})
 }
 
@@ -129,17 +184,14 @@ function updateVariant(req, res) {
 function updateVariantValue(req, res) {
 
 	let { updateVariantValues } = query.variant_values
-	let { updateSku } = query.sku
-	let { value_name, variant_id, price } = req.body
-	let { product_id } = req.params
+	let { value_name, variant_id } = req.body
+	let { value_id } = req.params
 
-	let sql = mysql.format(`${updateVariantValues} ${updateSku}`, [value_name, product_id, variant_id, price, product_id])
-
-	db.query(sql, (err, data) => {
+	db.query(updateVariantValues, [value_id, value_name, variant_id], (err, data) => {
 
 		if (err) return res.status(500).send({ message: `There was an error updating the product variant: ${err}` })
 
-		return res.status(500).send({ message: 'Product variant successfully updated' })
+		return res.status(200).send({ message: 'Product variant value successfully updated' })
 	})
 }
 
@@ -167,18 +219,20 @@ function removeVariantValue(req, res) {
 
 		if (err) return res.status(500).send({ message: `There was an error removing the product variant: ${err}` })
 
-		return res.status(500).send({ message: 'Product variant successfully removed' })
+		return res.status(200).send({ message: 'Product variant successfully removed' })
 	})
 }
 
 module.exports = {
 	getVariants,
 	getVariant,
+	getVariantValues,
 	getVariantsByProduct,
 	newVariant,
 	newSku,
-	addVariantValue,
+	newVariantValue,
 	updateVariant,
+	updateSku,
 	updateVariantValue,
 	removeVariantValue,
 	removeVariant
