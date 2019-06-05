b05f46e0efcd2c7f0d6c9d34fa03e600248a3cee
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-06-05 10:19:06 -0700

    Fix getting the property value of the result set of the stored procedure

--- app/middlewares/getValueNames.js
+++ app/middlewares/getValueNames.js
@@ -1,7 +1,8 @@
 const db = require('../db/db-connection')
+const query = require('../queries/queries')
 
 module.exports = function getValueIds(req, res, next) {
-	let { getVariantValuesByName } = req.variant_values
+	let { getVariantValuesByName } = query.variant_values
 	let { product_id } = req.params
 	let queryParams = req.query // value_names from the route params
 
@@ -13,16 +14,15 @@ module.exports = function getValueIds(req, res, next) {
 			db.query(getVariantValuesByName, [product_id, value], (err, data) => {
 
 				if (err) return res.status(500).send({ message: `Error getting value_id from variant_values: ${err}` }) //
-				console.log(data[0])
-				req.valuesId.push(data[0].value_id) // Store the value_id from the sql query
 
+				req.valuesId.push(data[0][0].value_id) // Store the value_id from the sql query
 			})
 		}
 	} else { // In case query params property is not an array
 		db.query(getVariantValuesByName, [product_id, queryParams.valueNames], (err, data) => {
 
 			if (err) return res.status(500).send({ message: `Error getting value_id from variant_values: ${err}` })
-			req.valuesId.push(data[0].value_id)
+			req.valuesId.push(data[0][0].value_id)
 
 		})
 	}
